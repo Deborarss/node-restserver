733e9ead3e13ab4fa58dd99e877bdc10f1d98409
Debora <debora_rss@hotmail.com>
2019-04-26 12:14:27 -0300

    user model

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let {Schema} = mongoose;      // let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'O nome é necessário']
+    },
+    email: {
+        type: String,
+        required: [true, 'O email é necessário']
+    },
+    password: {
+        type: String,
+        required: [true, 'A senha é obrigatória']
+    },
+    img: {
+        type: String,
+        required: false
+    },    
+    role: {
+        default: 'USER_ROLE'
+    },   // default: 'USER_ROLE'
+    state: {
+        type: Boolean,
+        default: true
+    },   
+    google: {
+        type: Boolean,
+        default: false
+    }   
+});
+
+module.exports = mongoose.model('User', userSchema);
